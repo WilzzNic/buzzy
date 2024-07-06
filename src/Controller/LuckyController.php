f5e96896765c788d61ebd5732b89046fb97467ad
Venox <wilzznic05@gmail.com>
2024-07-06 20:30:33 +0700

    Login form completed.

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class LuckyController extends AbstractController {
     
     #[Route('/lucky/number/{maxNumber}', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
     public function number(int $maxNumber = 100): Response {
         $number = random_int(0, $maxNumber);
 
