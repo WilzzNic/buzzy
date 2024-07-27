240a9d02d9666b4049369b6cf9c579c31550857b
Venox <wilzznic05@gmail.com>
2024-07-27 10:53:11 +0700

    Commit progress.

--- src/Controller/GroupChannelsController.php
+++ src/Controller/GroupChannelsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\GroupChannels;
 use App\Repository\GroupChannelsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,8 +11,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_USER')]
 class GroupChannelsController extends AbstractController {
-    #[Route('/group/channels', name: 'app_group_channels')]
 
+    #[Route('/group/channels', name: 'app_group_channels')]
     public function index(GroupChannelsRepository $repository): Response {
         $channels = $repository->findAll();
         return $this->render('group_channels/index.html.twig', [
